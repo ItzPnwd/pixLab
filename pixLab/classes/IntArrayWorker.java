ad1170e20a429b4b180deb701ea35c6ecf0f2a11
Donlin <sdonli4894@greenville.k12.sc.us>
2018-03-14 10:30:14 -0400

    updates, half way done

--- pixLab/classes/IntArrayWorker.java
+++ pixLab/classes/IntArrayWorker.java
@@ -60,6 +60,48 @@ public class IntArrayWorker
         count++;
       }
     }
+    }
+  
+  public int getCount(int finding)
+  {
+    int count = 0;
+    for (int row = 0; row < matrix.length; row++)
+    {
+      for (int col = 0; col < matrix[0].length; col++)
+      {
+        if(matrix[row][col] == finding)
+        {
+            count++;
+        }
+      }
+    }
+    return count;
+  }
+  
+  public int getLargest()
+  {
+    int largest = matrix[0][0];
+    for (int row = 0; row < matrix.length; row++)
+    {
+      for (int col = 0; col < matrix[0].length; col++)
+      {
+        if(matrix[row][col] >= largest)
+        {
+            largest = matrix[row][col];
+        }
+      }
+    }
+    return largest;
+  }
+  
+  public int getColTotal(int col)
+  {
+      int total = 0;
+      for (int row = 0; row < matrix.length; row++)
+      {
+          total += matrix[row][col];
+      }
+      return total;
   }
   
   /**
